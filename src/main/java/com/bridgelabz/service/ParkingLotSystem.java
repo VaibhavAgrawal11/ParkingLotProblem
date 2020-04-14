3f54a054e4f9a7b1ee435492da0e8fe7490d9125
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-04-14 22:02:03 +0530

    ADDED: Welcome message.

---
+++ src/main/java/com/bridgelabz/service/ParkingLotSystem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.service;
+
+public class ParkingLotSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to parking lot TDD problem.");
+    }
+}
