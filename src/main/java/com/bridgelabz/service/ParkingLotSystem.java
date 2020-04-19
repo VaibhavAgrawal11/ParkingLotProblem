dff8d9e9e02a1ca8b078db7eafac25850dfed428
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-04-19 15:33:35 +0530

    ADDED: Parking Bill entity to calculate user paking bill.

--- src/main/java/com/bridgelabz/service/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/service/ParkingLotSystem.java
@@ -4,6 +4,7 @@ import com.bridgelabz.exception.ParkingLotException;
 import com.bridgelabz.observer.Observer;
 import com.bridgelabz.observer.Subject;
 import com.bridgelabz.utilities.ParkingAttendant;
+import com.bridgelabz.utilities.ParkingBill;
 import com.bridgelabz.utilities.ParkingLot;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.Iterator;
 public class ParkingLotSystem implements Subject {
     ArrayList<Observer> observers = new ArrayList<Observer>();
     ParkingAttendant attendant;
+    ParkingBill parkingBill = new ParkingBill();
     int counter = 0;
     ParkingLot parkingLot;
     int lotCapacity;
@@ -40,7 +42,7 @@ public class ParkingLotSystem implements Subject {
         }
     }
 
-    public void parkVehicle(Object vehicle) throws ParkingLotException {
+    public void parkVehicle(Object vehicle, int arrivingHour) throws ParkingLotException {
         if (counter >= lotCapacity)
             throw new ParkingLotException("Parking lot is full.",
                     ParkingLotException.ExceptionType.NO_PARKING_AVAILABLE);
@@ -49,7 +51,7 @@ public class ParkingLotSystem implements Subject {
                     ParkingLotException.ExceptionType.VEHICLE_ALREADY_PRESENT);
         lotMap = attendant.parkVehicle(vehicle, lotMap);
         counter++;
-        System.out.println(lotMap);
+        parkingBill.arrivingHour(arrivingHour);
         this.notifyObservers();
     }
 
@@ -57,10 +59,11 @@ public class ParkingLotSystem implements Subject {
         return lotMap.containsValue(vehicle);
     }
 
-    public boolean unParkVehicle(Object vehicle, Integer parkingSlot) {
+    public boolean unParkVehicle(Object vehicle, Integer parkingSlot, int departingHour) {
         if (lotMap.containsValue(vehicle)) {
             lotMap.put(parkingSlot, null);
             counter--;
+            parkingBill.departureHour(departingHour);
             this.notifyObservers();
             return true;
         }
--- src/test/java/com/bridgelabz/servicetest/ParkingLotServiceTest.java
+++ src/test/java/com/bridgelabz/servicetest/ParkingLotServiceTest.java
@@ -5,6 +5,7 @@ import com.bridgelabz.service.ParkingLotSystem;
 import com.bridgelabz.utilities.AirportSecurityPersonal;
 import com.bridgelabz.utilities.Owner;
 import com.bridgelabz.utilities.ParkingAttendant;
+import com.bridgelabz.utilities.ParkingBill;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,40 +14,42 @@ public class ParkingLotServiceTest {
     ParkingLotSystem parkingLotSystem = null;
     Object vehicle = null;
     ParkingAttendant attendant = null;
+    ParkingBill parkingBill = null;
 
     @Before
     public void setUp() throws Exception {
         parkingLotSystem = new ParkingLotSystem(100);
         vehicle = new Object();
         attendant = new ParkingAttendant();
+        parkingBill = new ParkingBill();
     }
 
     @Test
     public void givenVehicle_BeenParked_ShouldReturnTrue() throws ParkingLotException {
-        parkingLotSystem.parkVehicle(vehicle);
+        parkingLotSystem.parkVehicle(vehicle,1);
         boolean isParked = parkingLotSystem.isVehicleParked(vehicle);
         Assert.assertTrue(isParked);
     }
 
     @Test
     public void givenVehicle_WhenUnParked_ShouldReturnTrue() throws ParkingLotException {
-        parkingLotSystem.parkVehicle(vehicle);
+        parkingLotSystem.parkVehicle(vehicle,1);
         Integer getSlot = attendant.getMyParkingSlot(vehicle);
-        boolean isUnParked = parkingLotSystem.unParkVehicle(vehicle, getSlot);
+        boolean isUnParked = parkingLotSystem.unParkVehicle(vehicle, getSlot,4);
         Assert.assertTrue(isUnParked);
     }
 
     @Test
     public void givenVehicle_WhenNotParked_ShouldNotBeUnParked() {
-        boolean isUnParked = parkingLotSystem.unParkVehicle(vehicle, attendant.getMyParkingSlot(vehicle));
+        boolean isUnParked = parkingLotSystem.unParkVehicle(vehicle, attendant.getMyParkingSlot(vehicle),4);
         Assert.assertFalse(isUnParked);
     }
 
     @Test
     public void givenVehicle_WhenAlreadyParked_ShouldNotBeParkedAgain() {
         try {
-            parkingLotSystem.parkVehicle(vehicle);
-            parkingLotSystem.parkVehicle(vehicle);
+            parkingLotSystem.parkVehicle(vehicle,1);
+            parkingLotSystem.parkVehicle(vehicle,2);
         } catch (ParkingLotException e) {
             Assert.assertEquals(ParkingLotException.ExceptionType.VEHICLE_ALREADY_PRESENT, e.type);
         }
@@ -56,12 +59,12 @@ public class ParkingLotServiceTest {
     public void givenVehicles_WhenParkingFull_ShouldThrowException() {
         ParkingLotSystem parkingLotSystem = new ParkingLotSystem(5);
         try {
-            parkingLotSystem.parkVehicle(vehicle);
-            parkingLotSystem.parkVehicle(new Object());
-            parkingLotSystem.parkVehicle(new Object());
-            parkingLotSystem.parkVehicle(new Object());
-            parkingLotSystem.parkVehicle(new Object());
-            parkingLotSystem.parkVehicle(new Object());
+            parkingLotSystem.parkVehicle(vehicle,1);
+            parkingLotSystem.parkVehicle(new Object(),1);
+            parkingLotSystem.parkVehicle(new Object(),1);
+            parkingLotSystem.parkVehicle(new Object(),1);
+            parkingLotSystem.parkVehicle(new Object(),1);
+            parkingLotSystem.parkVehicle(new Object(),1);
         } catch (ParkingLotException e) {
             Assert.assertEquals("Parking lot is full.", e.getMessage());
         }
@@ -70,8 +73,8 @@ public class ParkingLotServiceTest {
     @Test
     public void givenVehicles_WhenExactParkingIsDone_ShouldReturnTrue() throws ParkingLotException {
         ParkingLotSystem parkingLotSystem = new ParkingLotSystem(2);
-        parkingLotSystem.parkVehicle(new Object());
-        parkingLotSystem.parkVehicle(vehicle);
+        parkingLotSystem.parkVehicle(new Object(),1);
+        parkingLotSystem.parkVehicle(vehicle,1);
         boolean isParked = parkingLotSystem.isVehicleParked(vehicle);
         Assert.assertEquals(true, isParked);
     }
@@ -81,9 +84,9 @@ public class ParkingLotServiceTest {
         Owner owner = new Owner();
         ParkingLotSystem parkingLotSystem1 = new ParkingLotSystem(3);
         parkingLotSystem1.register(owner);
-        parkingLotSystem1.parkVehicle(vehicle);
-        parkingLotSystem1.parkVehicle(new Object());
-        parkingLotSystem1.parkVehicle(new Object());
+        parkingLotSystem1.parkVehicle(vehicle,1);
+        parkingLotSystem1.parkVehicle(new Object(),2);
+        parkingLotSystem1.parkVehicle(new Object(),3);
         Assert.assertEquals(owner.getSign(), Owner.Sign.PARKING_IS_FULL);
     }
 
@@ -92,9 +95,9 @@ public class ParkingLotServiceTest {
         AirportSecurityPersonal securityPersonal = new AirportSecurityPersonal();
         ParkingLotSystem parkingLotSystem1 = new ParkingLotSystem(3);
         parkingLotSystem1.register(securityPersonal);
-        parkingLotSystem1.parkVehicle(vehicle);
-        parkingLotSystem1.parkVehicle(new Object());
-        parkingLotSystem1.parkVehicle(new Object());
+        parkingLotSystem1.parkVehicle(vehicle,2);
+        parkingLotSystem1.parkVehicle(new Object(),1);
+        parkingLotSystem1.parkVehicle(new Object(),2);
         Assert.assertEquals(true, securityPersonal.redirectSecurityStaff());
     }
 
@@ -103,8 +106,8 @@ public class ParkingLotServiceTest {
         AirportSecurityPersonal securityPersonal = new AirportSecurityPersonal();
         ParkingLotSystem parkingLotSystem1 = new ParkingLotSystem(3);
         parkingLotSystem1.register(securityPersonal);
-        parkingLotSystem1.parkVehicle(vehicle);
-        parkingLotSystem1.parkVehicle(new Object());
+        parkingLotSystem1.parkVehicle(vehicle,1);
+        parkingLotSystem1.parkVehicle(new Object(),2);
         Assert.assertEquals(false, securityPersonal.redirectSecurityStaff());
     }
 
@@ -113,10 +116,18 @@ public class ParkingLotServiceTest {
         Owner owner = new Owner();
         ParkingLotSystem parkingLotSystem1 = new ParkingLotSystem(3);
         parkingLotSystem1.register(owner);
-        parkingLotSystem1.parkVehicle(vehicle);
-        parkingLotSystem1.parkVehicle(new Object());
-        parkingLotSystem1.parkVehicle(new Object());
-        parkingLotSystem1.unParkVehicle(vehicle, attendant.getMyParkingSlot(vehicle));
+        parkingLotSystem1.parkVehicle(vehicle,1);
+        parkingLotSystem1.parkVehicle(new Object(),2);
+        parkingLotSystem1.parkVehicle(new Object(),2);
+        parkingLotSystem1.unParkVehicle(vehicle, attendant.getMyParkingSlot(vehicle),3);
         Assert.assertEquals(owner.getSign(), null);
+    }
+
+    @Test
+    public void givenParkingTime_ParkingBillShouldBeGenerated() throws ParkingLotException {
+        parkingLotSystem.parkVehicle(vehicle,1);
+        Integer parkingSlot = attendant.getMyParkingSlot(vehicle);
+        parkingLotSystem.unParkVehicle(vehicle,parkingSlot,4);
+        Assert.assertEquals(7.5,parkingBill.generateParkingBill(),0);
     }
 }
